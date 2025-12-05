7877bc6517a0ffd09df8f2cea62e6e2a79f0431d
Kantemir <kan.shandir94@gmail.com>
2025-12-05 18:57:21 +0300

    fix cors

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://runner-frontend.vercel.app/"],
+    allow_origins=["https://runner-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
